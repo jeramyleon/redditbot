9f5cacfbbb528b7206b5d83175f76ed876461065
Luis <mikeylopez@gmail.com>
2015-05-13 17:14:33 -0400

    bot is now a class and could read settings.json

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,29 +1,37 @@
-#require 'pry'
 require 'httparty'
+require 'pry'
+require 'json'
 
 class Bot
 
-resp = HTTParty.get("http://reddit.com/r/nba/.json")
+  include HTTParty
 
-def search(input,resp)
-
-attr_accessor :resp
-
-prefix = "http://reddit.com/" 
-
-input = ARGV[0].split(/,/)
-resp["data"]["children"].each do |story|
-  input.each do |term|
-  #  binding.pry
-   if story["data"]["title"].downcase.include?(term.strip.downcase)
-    puts " " 
-    puts "#{term.strip.downcase} matches:"
-    puts "title: " + story["data"]["title"]
-    puts "url: " + story["data"]["url"]
-    puts "reddit url: " + prefix +story["data"]["permalink"]
-   end
+  def initialize(settings)
+    @settings = settings
   end
-end
+
+  def search
+    @settings["sub-reddits"].each do |key, val|
+      puts "In #{key} sub-reddit looking for terms #{val["terms"]}:"
+      reddit_data = self.class.get(val["url"])
+      reddit_data["data"]["children"].each do |story|
+        val["terms"].split(/,/).each do |term|
+          if story["data"]["title"].downcase.include?(term.strip.downcase)
+            puts " "
+            puts "#{term.strip.downcase} matches:"
+            puts "title: " + story["data"]["title"]
+            puts "url: " + story["data"]["url"]
+            puts "reddit url: " + prefix + story["data"]["permalink"]
+          end
+        end
+      end
+    end
+  end
+
 end
 
-  
+#JSON.parse turns settings.json into a hash if its a hash and array if its an array
+settings = JSON.parse(File.read("settings.json"))
+
+@bot = Bot.new(settings)
+@bot.search
